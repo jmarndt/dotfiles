2cc3273587dc36cb767961427b5ade54c7ebae15
Joe Arndt <jmarndt@users.noreply.github.com>
2022-08-05 20:36:51 -0500

    restructured folders

--- README.md
+++ README.md
@@ -2,6 +2,5 @@
 This repo holds my custom program configuraiton files.
 
 # install dotfiles
-Simply run the `propagate_dotfiles.sh` script included in this repo.
-The script will backup any existing configurations if they exist, then link the respective config files to the ones in this repo.
-It will also create a file in the home directory with an export for the path of this repo on the local machine. This file is sourced at the beginning of the shell rc file so that various aliases work properly, and so that the location of the repo is irrelevant and can be moved at any time. just rerun the propagate script anytime the location of the files changes for this to be properly updated.
+Simply run the `propagate_dotfiles.sh` script included in this repo. This will backup any existing configurations if they exist, then link the respective config files to the ones in this repo.
+It will also create an export for the path of this repo on the local machine that is sourced at the beginning of the shell rc file so that various aliases work properly, and so that the path of the repo on the local machine is irrelevant.
--- propagate_dotfiles.sh
+++ propagate_dotfiles.sh
@@ -3,37 +3,40 @@
 # Set variables
 DOTFILES_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 DF_DEST=$HOME/.df
-SHELLRC_SOURCE=$DOTFILES_DIR/zsh/zshrc
-SHELLRC_DEST=$HOME/.zshrc
-VIMRC_SOURCE=$DOTFILES_DIR/vim/vimrc
-VIMRC_DEST=$HOME/.vimrc
-TMUX_SOURCE=$DOTFILES_DIR/tmux/tmux.conf
-TMUX_DEST=$HOME/.tmux.conf
-GIT_SOURCE=$DOTFILES_DIR/git/gitconfig
-GIT_DEST=$HOME/.gitconfig
+
+#SHELLRC_SOURCE=$DOTFILES_DIR/zsh/zshrc
+#VIMRC_SOURCE=$DOTFILES_DIR/vim/vimrc
+#TMUX_SOURCE=$DOTFILES_DIR/tmux/tmux.conf
+#GIT_SOURCE=$DOTFILES_DIR/git/gitconfig
+#SHELLRC_DEST=$HOME/.zshrc
+#VIMRC_DEST=$HOME/.vimrc
+#TMUX_DEST=$HOME/.tmux.conf
+#$HOME/$GIT_CONF=$HOME/.gitconfig
+
+SHELLRC=.zshrc
+VIMRC=.vimrc
+TMUX_CONF=.tmux.conf
+GIT_CONF=.gitconfig
 
 # Shell RC 
-[ -L "$SHELLRC_DEST" ] && rm $SHELLRC_DEST
-[ -f "$SHELLRC_DEST" ] && mv $SHELLRC_DEST $SHELLRC_DEST.bak.$(date +%m%d%Y-%H%M%S)
-ln -s $SHELLRC_SOURCE $SHELLRC_DEST
+[ -L "$HOME/$SHELLRC" ] && rm $HOME/$SHELLRC
+[ -f "$HOME/$SHELLRC" ] && mv $HOME/$SHELLRC $HOME/$SHELLRC.bak.$(date +%m%d%Y-%H%M%S)
+ln -s $DOTFILES_DIR/$SHELLRC $HOME/$SHELLRC
 
 # Vim RC
-[ -L "$VIMRC_DEST" ] && rm $VIMRC_DEST
-[ -f "$VIMRC_DEST" ] && mv $VIMRC_DEST $VIMRC_DEST.bak.$(date +%m%d%Y-%H%M%S)
-ln -s $VIMRC_SOURCE $VIMRC_DEST
+[ -L "$HOME/$VIMRC" ] && rm $HOME/$VIMRC
+[ -f "$HOME/$VIMRC" ] && mv $HOME/$VIMRC $HOME/$VIMRC.bak.$(date +%m%d%Y-%H%M%S)
+ln -s $DOTFILES_DIR/$VIMRC $HOME/$VIMRC
 
 # Tmux Conf
-[ -L "$TMUX_DEST" ] && rm $TMUX_DEST
-[ -f "$TMUX_DEST" ] && mv $TMUX_DEST $TMUX_DEST.bak.$(date +%m%d%Y-%H%M%S)
-ln -s $TMUX_SOURCE $TMUX_DEST
+[ -L "$HOME/$TMUX_CONF" ] && rm $HOME/$TMUX_CONF
+[ -f "$HOME/$TMUX_CONF" ] && mv $HOME/$TMUX_CONF $HOME/$TMUX_CONF.bak.$(date +%m%d%Y-%H%M%S)
+ln -s $DOTFILES_DIR/$TMUX_CONF $HOME/$TMUX_CONF
 
 # Git Config
-[ -L "$GIT_DEST" ] && rm $GIT_DEST
-[ -f "$GIT_DEST" ] && mv $GIT_DEST $GIT_DEST.bak.$(date +%m%d%Y-%H%M%S)
-ln -s $GIT_SOURCE $GIT_DEST
+[ -L "$HOME/$GIT_CONF" ] && rm $HOME/$GIT_CONF
+[ -f "$HOME/$GIT_CONF" ] && mv $HOME/$GIT_CONF $HOME/$GIT_CONF.bak.$(date +%m%d%Y-%H%M%S)
+ln -s $DOTFILES_DIR/$GIT_CONF $HOME/$GIT_CONF
 
-# Update dotfiles location
+# Update dotfiles directory location
 echo "export DOTFILES_DIR=\"$DOTFILES_DIR\"" > $DF_DEST
-
-# Finish
-echo "dotfiles installed! don't forget to source your rc files!"
