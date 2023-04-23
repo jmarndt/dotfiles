0205cc409f39771be4940e0c343127b9ddc60774
Joe Arndt <jmarndt@users.noreply.github.com>
2023-04-23 10:48:09 -0500

    updated file structure

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 DOTFILES_DIR=~/dotfiles
-SHELLRC=.zshrc
-VIMRC=.vimrc
-GIT_CONF=.gitconfig
+SHELLRC=zshrc
+VIMRC=vimrc
+GIT_CONF=gitconfig
 
-[ -L ~/$SHELLRC ] && rm ~/$SHELLRC
-[ -f ~/$SHELLRC ] && rm ~/$SHELLRC
-ln -s $DOTFILES_DIR/$SHELLRC ~/$SHELLRC
+[ -L ~/.$SHELLRC ] && rm ~/.$SHELLRC
+[ -f ~/.$SHELLRC ] && rm ~/.$SHELLRC
+ln -s $DOTFILES_DIR/$SHELLRC ~/.$SHELLRC
 
-[ -L ~/$VIMRC ] && rm ~/$VIMRC
-[ -f ~/$VIMRC ] && rm ~/$VIMRC
-ln -s $DOTFILES_DIR/$VIMRC ~/$VIMRC
+[ -L ~/.$VIMRC ] && rm ~/.$VIMRC
+[ -f ~/.$VIMRC ] && rm ~/.$VIMRC
+ln -s $DOTFILES_DIR/$VIMRC ~/.$VIMRC
 
-[ -L ~/$GIT_CONF ] && rm ~/$GIT_CONF
-[ -f ~/$GIT_CONF ] && rm ~/$GIT_CONF
-ln -s $DOTFILES_DIR/$GIT_CONF ~/$GIT_CONF
+[ -L ~/.$GIT_CONF ] && rm ~/.$GIT_CONF
+[ -f ~/.$GIT_CONF ] && rm ~/.$GIT_CONF
+ln -s $DOTFILES_DIR/$GIT_CONF ~/.$GIT_CONF
