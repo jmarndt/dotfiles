b55652cabbde4af6fcb851dbf909015c8b6741c5
Joe Arndt <jmarndt@users.noreply.github.com>
2023-04-26 14:16:13 -0500

    added sqlite and refactored dotfile script

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 DOTFILES_DIR=$HOME/dotfiles
 VIMRC=vimrc
+SQLITERC=sqliterc
 GIT_CONF=gitconfig
 ZSHRC=zshrc
 SOURCE="source $DOTFILES_DIR/$ZSHRC"
 
-[ -L $HOME/.$VIMRC ] && rm $HOME/.$VIMRC
-[ -f $HOME/.$VIMRC ] && rm $HOME/.$VIMRC
-ln -s $DOTFILES_DIR/$VIMRC $HOME/.$VIMRC
+link_dotfile() {
+    dotfile=$1
+    [ -L $HOME/.$dotfile ] && rm $HOME/.$dotfile
+    [ -f $HOME/.$dotfile ] && rm $HOME/.$dotfile
+    ln -s $DOTFILES_DIR/$dotfile $HOME/.$dotfile
+}
 
-[ -L $HOME/.$GIT_CONF ] && rm $HOME/.$GIT_CONF
-[ -f $HOME/.$GIT_CONF ] && rm $HOME/.$GIT_CONF
-ln -s $DOTFILES_DIR/$GIT_CONF $HOME/.$GIT_CONF
+link_dotfile $VIMRC
+link_dotfile $SQLITERC
+link_dotfile $GIT_CONF
 
 [ -f $HOME/$ZSHRC ] && grep -v "$SOURCE" $HOME/.$ZSHRC > temp && mv temp $HOME/.$ZSHRC
 echo $SOURCE >> $HOME/.$ZSHRC
